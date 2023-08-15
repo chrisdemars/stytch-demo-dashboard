8d9cde45fd64fa45ecb242ccc629f7aa259b004b
Chris <chris@chrisdemars.com>
2023-08-15 15:07:27 -0400

    adds new login features

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <header className=''></header>
       <h1>Welcome to the Stytch login</h1>
       <Login />
+      <a
+        href='https://test.stytch.com/v1/public/oauth/google/start?public_token=public-token-test-49f607c3-8c98-4779-8592-16393e21855e'
+        target='_blank'
+        rel='noopener noreferrer'>
+        LOGIN
+      </a>
     </div>
   );
 }
